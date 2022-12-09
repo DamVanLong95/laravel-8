f7bccb34c8295d24574ac4fa35516e096172e8f6
longdv <long.dam@kiaisoft.com>
2022-12-09 11:07:59 +0700

    telescope, adminer db

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -44,6 +44,14 @@ class Kernel extends HttpKernel
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
+
+        'adminer' => [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+	        // you may create customized middleware to fit your needs
+            \Illuminate\Auth\Middleware\Authenticate::class,
+	    ],
     ];
 
     /**
